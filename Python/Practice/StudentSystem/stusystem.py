3ca54cada655e9ec5fdcf880adb6587dba85a4ed
ls <1946754214@qq.com>
2021-04-02 15:30:11 +0800

    add studentsystem

--- Python/Practice/StudentSystem/stusystem.py
+++ Python/Practice/StudentSystem/stusystem.py
@@ -77,8 +77,56 @@ def save(lst):
     stu_txt.close()
 
 def search():
-    pass
-
+    student_quert=[]
+    while True:
+        id=''
+        name=''
+        if os.path.exists(filename):
+            mode=input('按ID查询请输入1，按姓名查找请输入2：')
+            if mode=='1':
+                id=input('请输入学生ID：')
+            elif mode=='2':
+                name=input('请输入学生姓名：')
+            else:
+                print('你输入的信息有误，请重新输入')
+                continue
+            with open(filename,'r',encoding='utf-8') as rfile:
+                student=rfile.readlines()
+                for item in student:
+                    d=dict(eval(item))
+                    if id!='':
+                        if d['id']==id:
+                            student_quert.append(d)
+                    elif name!='':
+                        if d['name']==name:
+                            student_quert.append(d)
+            show_student(student_quert)
+            student_quert.clear()
+            answer=input('是否要继续查询？y/n\n')
+            if answer=='y':
+                continue
+            else:
+                break
+        else:
+            print('暂未保存学生信息')
+            return
+def show_student(lst):
+    if len(lst)==0:
+        print('没有查询到学生信息，无数据显示！！')
+        return
+    # 定义标题显示格式
+    format_title='{:^6}\t{:^12}\t{:^8}\t{:^10}\t{:^10}\t{:^8}'
+    print(format_title.format('ID','姓名','英语成绩','Python成绩','java成绩','总成绩'))
+    # 定义内容显示格式
+    format_data = '{:^6}\t{:^12}\t{:^8}\t{:^10}\t{:^10}\t{:^8}'
+    for item in lst:
+        print(format_data.format(item.get('id'),
+                                 item.get('name'),
+                                 item.get('english'),
+                                 item.get('python'),
+                                 item.get('java'),
+                                 int(item.get('english'))+int(item.get('python'))+int(item.get('java'))
+                                 ))
 def delete():
     while True:
         student_id=input('请输入要删除的学生的ID：')
@@ -145,12 +193,62 @@ def modify():
 
 
 def sort():
-    pass
+    show()
+    if os.path.exists(filename):
+        with open(filename,'r',encoding='utf-8') as rfile:
+            student_list = rfile.readlines()
+            students_new=[]
+            for item in student_list:
+                d=dict(eval(item))
+                students_new.append(d)
+    else:
+        return
+    asc_or_dasc=input('请选择（0：升序，1：降序)')
+    if asc_or_dasc=='0':
+        asc_or_dasc_bool=False
+    elif asc_or_dasc=='1':
+        asc_or_dasc_bool=True
+    else:
+        print('你的输入有误，请重新输入！！')
+        sort()
+    mode =input('请输入排序方式（1.按英语成绩排序 2.按Python成绩排序 3.按Java成绩排序 0.按总成绩排序）')
+    if mode=='1':
+        students_new.sort(key=lambda x:int(x['english']),reverse=asc_or_dasc_bool)
+    elif mode=='2':
+        students_new.sort(key=lambda x: int(x['python']), reverse=asc_or_dasc_bool)
+    elif mode=='3':
+        students_new.sort(key=lambda x: int(x['java']), reverse=asc_or_dasc_bool)
+    elif mode=='4':
+        students_new.sort(key=lambda x: int(x['english'])+int(x['python'])+int(x['java']), reverse=asc_or_dasc_bool)
+    else:
+        print('您输入的信息有误请重新输入！！！')
+        sort()
+    show_student(students_new)
+
+
 
 def total():
-    pass
+    if os.path.exists(filename):
+        with open(filename,'r',encoding='utf-8') as rfile:
+            student = rfile.readlines()
+            if student:
+                print(f'一共有{len(student)}名学生')
+            else:
+                print('还没有录入学生信息！！')
+    else:
+        print('暂未保存数据信息。。。。')
+
 def show():
-    pass
+    student_list=[]
+    if os.path.exists(filename):
+        with open(filename,'r',encoding='utf-8') as rfile:
+            students = rfile.readlines()
+            for item in students:
+                student_list.append(eval(item))
+            if student_list:
+                show_student(student_list)
+    else:
+        print('暂未保存数据信息。。。。')
 
 if __name__ == '__main__':
     main()
